f98b194ee7a39d18e54558643e9eed9bba5cdf88
Chano Vera <github@chanovera.com>
2025-11-11 14:21:22 -0600

    add the header and footer sections

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <header id="main-header" role="banner" aria-label="<?= esc_attr__( 'Main header', 'core' ); ?>"></header>
