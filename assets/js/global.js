b887e19b7705471bea6a92d6a34d87d4b308b10c
Chano Vera <github@chanovera.com>
2025-11-11 19:30:39 -0600

    first support for forms and hybrid blocks

---
+++ assets/js/global.js
@@ -0,0 +1,10 @@
+// Detect Chromium-based browsers
+const ua = navigator.userAgent.toLowerCase();
+const isChromium =
+  !!window.chrome && /chrome|crios|crmo|edg|brave|opera|opr|vivaldi/i.test(ua);
+
+if (isChromium) {
+  document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('is-chromium');
+  });
+}
